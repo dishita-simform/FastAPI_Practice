226d9c04e956287d771e459b02ed14b85cbac991
dishita-simform <dishita.tank@simformsolutions.com>
2025-05-09 15:26:56 +0530

    Request Body with Path & Query Parameters

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import Optional
 
 router = APIRouter(
     prefix="/blog",
@@ -8,3 +10,17 @@ router = APIRouter(
 @router.post('/create')
 def create_blog():
     return {'message' : 'Blog Created!'}
+
+class BlogModel(BaseModel):
+    title:str
+    content:str
+    published:Optional[bool] = True
+
+@router.post('/new/{id}')
+def create_blog (blog:BlogModel, id:int, version: int=1):
+    return {
+        'message' : 'Blog Created!',
+        'id' : id,
+        'version' : version,
+        'data' : blog
+    }
